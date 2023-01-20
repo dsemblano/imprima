6787e3583bcb682008c6c8b512c1d12d239aeec4
Daniel Semblano <dsemblano@gmail.com>
2023-01-20 20:01:22 -0300

    section tag to checkout and cart pages

--- site/web/app/themes/nosteefty/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/nosteefty/resources/views/partials/content-page.blade.php
@@ -1,7 +1,7 @@
 
 @php $page = (! is_page(['checkout', 'cart'])) ? "article" : "section" @endphp
 
-<{{$page}} class="prose md:prose-lg lg:prose-xl mx-auto max-w-none bg-red-600">
+<{{$page}} class="prose md:prose-lg lg:prose-xl mx-auto max-w-none">
     @php(the_content())
 </{{$page}}>
 
