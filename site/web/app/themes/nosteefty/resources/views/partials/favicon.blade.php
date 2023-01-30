581ed372bb0a874a6d22d3845388375dec5a3bc9
Daniel Semblano <dsemblano@gmail.com>
2023-01-30 19:17:56 -0300

    New favicons fix

--- site/web/app/themes/nosteefty/resources/views/partials/favicon.blade.php
+++ site/web/app/themes/nosteefty/resources/views/partials/favicon.blade.php
@@ -1,9 +1,20 @@
-<link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
-<link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
-<link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
-<link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
-<meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
-<link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab_9f74bc90.svg" color="#e2aa9f')">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="@asset('images/favicon/apple-touch-icon-57x57.png')">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="@asset('images/favicon/apple-touch-icon-114x114.png')">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="@asset('images/favicon/apple-touch-icon-72x72.png')">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="@asset('images/favicon/apple-touch-icon-144x144.png')">
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="@asset('images/favicon/apple-touch-icon-60x60.png')">
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="@asset('images/favicon/apple-touch-icon-120x120.png')">
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="@asset('images/favicon/apple-touch-icon-76x76.png')">
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="@asset('images/favicon/apple-touch-icon-152x152.png')">
+<link rel="icon" type="image/png" href="@asset('images/favicon/favicon-196x196.png" sizes="196x196')">
+<link rel="icon" type="image/png" href="@asset('images/favicon/favicon-96x96.png" sizes="96x96')">
+<link rel="icon" type="image/png" href="@asset('images/favicon/favicon-32x32.png" sizes="32x32')">
+<link rel="icon" type="image/png" href="@asset('images/favicon/favicon-16x16.png" sizes="16x16')">
+<link rel="icon" type="image/png" href="@asset('images/favicon/favicon-128.png" sizes="128x128')">
+<meta name="application-name" content="NOS teefty - Imprima Sua Camiseta"/>
+<meta name="msapplication-TileColor" content="#FFFFFF')">
+<meta name="msapplication-TileImage" content="@asset('images/favicon/mstile-144x144.png')">
+<meta name="msapplication-square70x70logo" content="@asset('images/favicon/mstile-70x70.png')">
+<meta name="msapplication-square150x150logo" content="@asset('images/favicon/mstile-150x150.png')">
+<meta name="msapplication-wide310x150logo" content="@asset('images/favicon/mstile-310x150.png')">
+<meta name="msapplication-square310x310logo" content="@asset('images/favicon/mstile-310x310.png')">
