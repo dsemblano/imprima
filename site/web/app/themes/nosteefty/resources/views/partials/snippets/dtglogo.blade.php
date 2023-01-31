3e0cd1044eda4726b9c5752aa738073b9061136f
Daniel Semblano <dsemblano@gmail.com>
2023-01-31 17:32:05 -0300

    dtg logo height fixed

--- site/web/app/themes/nosteefty/resources/views/partials/snippets/dtglogo.blade.php
+++ site/web/app/themes/nosteefty/resources/views/partials/snippets/dtglogo.blade.php
@@ -1,3 +1,3 @@
 <a href="/design-editor">
-    <img class="w-full h-16" src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" width="271" height="238" />
+    <img id="dtg" class="w-full h-16 md:h-full" src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" width="271" height="238" />
 </a>
--- site/web/app/themes/nosteefty/resources/views/partials/snippets/logo.blade.php
+++ site/web/app/themes/nosteefty/resources/views/partials/snippets/logo.blade.php
@@ -1,3 +1,3 @@
 <a class="brand" href="{{ home_url('/') }}">
-    <img class="w-full" src="@asset('images/logo_imprima.png')" alt="Logo site" width="200" height="107" />
+    <img class="w-full h-10 md:h-full" src="@asset('images/logo_imprima.png')" alt="Logo site" width="200" height="107" />
 </a>
