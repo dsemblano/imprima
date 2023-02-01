2514588cf9d8ffbffc2570ec9d19db7c039e91c7
Daniel Semblano <dsemblano@gmail.com>
2023-02-01 02:02:20 -0300

    background-image, menu on from sm to xl resolutions fixed v1.3

--- site/web/app/themes/nosteefty/resources/views/partials/snippets/dtglogo.blade.php
+++ site/web/app/themes/nosteefty/resources/views/partials/snippets/dtglogo.blade.php
@@ -1,3 +1,3 @@
 <a href="/design-editor">
-    <img id="dtg" class="w-full h-14 lg:h-16 xl:h-full" src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" width="271" height="238" />
+    <img id="dtg" class="w-full h-16 md:h-24 xl:h-full" src="@asset('images/imprima_sua_ideia.png')" alt="Logo site" width="271" height="238" />
 </a>
